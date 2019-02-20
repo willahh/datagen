dc9d0943c420f3d5c8092b1d8f45f52a73f86d17
William Ravel <wravel@gmail.com>
2019-02-21 00:25:49 +0100

    Update core.cljs

--- src/datalog_schema/core.cljs
+++ src/datalog_schema/core.cljs
@@ -81,6 +81,9 @@
    [:p "Here we can define some logic rules"]
    [:h3 "Generate some helper methods"]
    [:p "Here we can define some common helper methods for the domain."]
+   [:p "-----> Graph preview on side (graphiz)"]
+   [:p "----> Repl"]
+   [:p "----> clojure.spec gen"]
    [:table.table
     [:tbody
      [:tr
@@ -95,12 +98,7 @@
                             (. js/document (getElementById "app"))))
 
 (defn ^:export init []
-  ;; init is called ONCE when the page loads
-  ;; this is called in the index.html and must be exported
-  ;; so it is available even in :advanced release builds
   (start))
 
 (defn stop []
-  ;; stop is called before any code is reloaded
-  ;; this is controlled by :before-load in the config
   (js/console.log "stop"))
